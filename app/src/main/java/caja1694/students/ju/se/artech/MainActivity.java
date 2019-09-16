85846e017f53be3b93de0c5d539e2e6b2dc2dde4
Jake <jacob.carlquist@hotmail.com>
2019-09-16 15:19:18 +0200

    First commit of the project

---
+++ app/src/main/java/caja1694/students/ju/se/artech/MainActivity.java
@@ -0,0 +1,13 @@
+package caja1694.students.ju.se.artech;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
