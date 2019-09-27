e1bc1a67337279681c538a0b50c1d50c96d54518
Jake <jacob.carlquist@hotmail.com>
2019-09-27 13:22:58 +0200

    SystemWideWarnings

--- app/src/main/java/caja1694/students/ju/se/artech/MainActivity.java
+++ app/src/main/java/caja1694/students/ju/se/artech/MainActivity.java
@@ -1,5 +1,6 @@
 package caja1694.students.ju.se.artech;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -12,9 +13,13 @@ import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -35,21 +40,28 @@ public class MainActivity extends AppCompatActivity {
 
     final static String NuclearTechnician = "NuclearTechnician1";
 
+    final static String OverstayedStatus = "OverstayedStatus";
+
+
     String currentTime = LocalTime.now().toString();
     String currentDate = LocalDate.now().toString();
 
     DatabaseReference myRef =  FirebaseDatabase.getInstance().getReference();
+    DatabaseReference statusReference = myRef.child(currentDate).child(NuclearTechnician).child("Status");
+
     Boolean isClockedIn = false;
     CountDownTimer mCountDownTimer;
 
     private NotificationManagerCompat notificationManager;
 
-    TimeKeeper timeKeeper = new TimeKeeper(605000);
+    TimeKeeper timeKeeper = new TimeKeeper(5000);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        valueEventListener();
 
         final Button clockInButton = findViewById(R.id.clock_in_button);
 
@@ -86,9 +98,8 @@ public class MainActivity extends AppCompatActivity {
             }
             @Override
             public void onFinish(){
-                sendWarning(systemWideWarning);
                 if(isClockedIn){
-                    myRef.child(currentDate).child(NuclearTechnician).child("Status").setValue("overstayed");
+                    statusReference.setValue(OverstayedStatus);
                 }
             }
         }.start();
@@ -96,13 +107,16 @@ public class MainActivity extends AppCompatActivity {
 
     void stopTimer(){
         mCountDownTimer.cancel();
-        timeKeeper.setTimeLeftInMillis(timeLeft());
+        timeKeeper.setTimeLeftInMillis(timeUntilRadiationLimit());
     }
 
-    long timeLeft(){
+    long timeUntilRadiationLimit(){
         Radiation radiationLevels = new Radiation(30, REACTOR_ROOM, 1);
+
         double exposurePerSecond = radiationLevels.getUnitExposurePerSecond();
+
         double exposurePerMiliSecond = exposurePerSecond/1000;
+
         double timeLeft = 500000/exposurePerMiliSecond;
         return (long) timeLeft;
     }
@@ -129,16 +143,34 @@ public class MainActivity extends AppCompatActivity {
         TextView textView = findViewById(R.id.status);
         textView.setText("Clocked in");
         myRef.child(currentDate).child(NuclearTechnician).child("Clocked in").push().setValue(currentTime);
-        myRef.child(currentDate).child(NuclearTechnician).child("Status").setValue("active");
+       statusReference.setValue("active");
         startTimer();
-
     }
     void clockOut(){
         isClockedIn = false;
         TextView textView = findViewById(R.id.status);
         textView.setText("Clocked Out");
         myRef.child(currentDate).child(NuclearTechnician).child("Clocked out").push().setValue(currentTime);
-        myRef.child(currentDate).child(NuclearTechnician).child("Status").setValue("inactive");
+        statusReference.setValue("inactive");
         stopTimer();
+    }
+
+    void valueEventListener(){
+        ValueEventListener statusListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String technichianStatus = dataSnapshot.getValue().toString();
+
+                if(technichianStatus.equals(OverstayedStatus)) {
+                    sendWarning(systemWideWarning);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Database error: "+databaseError, Toast.LENGTH_SHORT).show();
+            }
+        };
+        statusReference.addValueEventListener(statusListener);
     }
 }
--- app/src/main/java/caja1694/students/ju/se/artech/Radiation.java
+++ app/src/main/java/caja1694/students/ju/se/artech/Radiation.java
@@ -1,42 +1,42 @@
 package caja1694.students.ju.se.artech;
 
 public class Radiation {
-    private double R;
-    private double rc;
-    private double pc;
+    private double reactorOutputPerSecond;
+    private double roomCoefficient;
+    private double protectionCoefficient;
 
 
-    public Radiation(double R, double rc, double pc){
-        this.R = R;
-        this.rc = rc;
-        this.pc = pc;
+    public Radiation(double reactorOutputPerSecond, double roomCoefficient, double protectionCoefficient){
+        this.reactorOutputPerSecond = reactorOutputPerSecond;
+        this.roomCoefficient = roomCoefficient;
+        this.protectionCoefficient = protectionCoefficient;
     }
 
-    public double getR() {
-        return R;
-    }
-
-    public void setR(double r) {
-        this.R = r;
-    }
-
-    public double getPc() {
-        return pc;
-    }
-
-    public void setPc(double pc) {
-        this.pc = pc;
-    }
-
-    public double getRc() {
-        return rc;
-    }
-
-    public void setRc(double rc) {
-        this.rc = rc;
-    }
     public double getUnitExposurePerSecond(){
-        double E = (this.R*this.rc)/this.pc;
-        return E;
+        return (this.reactorOutputPerSecond * this.roomCoefficient) / this.protectionCoefficient;
+    }
+
+    public double getReactorOutputPerSecond() {
+        return reactorOutputPerSecond;
+    }
+
+    public void setReactorOutputPerSecond(double reactorOutputPerSecond) {
+        this.reactorOutputPerSecond = reactorOutputPerSecond;
+    }
+
+    public double getProtectionCoefficient() {
+        return protectionCoefficient;
+    }
+
+    public void setProtectionCoefficient(double protectionCoefficient) {
+        this.protectionCoefficient = protectionCoefficient;
+    }
+
+    public double getRoomCoefficient() {
+        return roomCoefficient;
+    }
+
+    public void setRoomCoefficient(double roomCoefficient) {
+        this.roomCoefficient = roomCoefficient;
     }
 }
